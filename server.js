743f095e3518771cf47a584dce45829212636306
louis <louisdrippe@gmail.com>
2024-07-09 18:25:51 -0500

    hopefully fixes problems

--- server.js
+++ server.js
@@ -11,16 +11,14 @@ const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
-    secret: process.env.DB_SECRET,
+    secret: 'Super secret secret',
     cookie: {},
     resave: false,
     saveUninitialized: true,
     store: new SequelizeStore({
-        db: sequelize,
-        checkExpirationInterval: 1000 * 60 * 10, // will check every 10 minutes
-        expiration: 1000 * 60 * 30 // will expire after 30 minutes
+      db: sequelize
     })
-};
+  };
 
 const app = express();
 const PORT = process.env.PORT || 3001;
